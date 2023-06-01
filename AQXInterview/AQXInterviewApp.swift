eb6746fbf231f952ad2a7c3c49e0396852c857f1
Russell <rwjk16@gmail.com>
2023-06-01 14:00:41 -0500

    Initial Commit

---
+++ AQXInterview/AQXInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQXInterviewApp.swift
+//  AQXInterview
+//
+//  Created by Russell Weber on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct AQXInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
